01c2143184b5a0640902e970542c3ba72e93caca
Scubaboy <robin@robincawsey.plus.com>
2015-05-29 09:21:00 +0100

    added uncommited changes

---
+++ QuadWebApi/Controllers/Smtp/SmtpController.cs
@@ -0,0 +1,27 @@
+﻿using QuadWebApi.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace QuadWebApi.Controllers.Smtp
+{
+    [RoutePrefix("api/smtp")]
+    public class SmtpController : ApiController
+    {
+        private ISmtp isMe;
+        public SmtpController(ISmtp me)
+        {
+            this.isMe = me;
+
+        }
+
+        [Route("smtpTest/{id}")]
+        [HttpGet]
+        public string test(string id)
+        {
+            return this.isMe.Test() + id;
+        }
+    }
+}
---
+++ QuadWebApi/SignalRHubs/NotifyUpdate/NotifyUpdateHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace QuadWebApi.SignalRHubs.NotifyUpdate
+{
+    public class NotifyUpdateHub : Hub
+    {
+        public void DataUpdateNotification(string updateType, string updateScope)
+        {
+            if (updateScope == "All")
+            {
+                Clients.All.dataChanged(updateType);
+            }
+            else
+            {
+                Clients.Others.dataChanged(updateType);
+            }
+        }
+    }
+}
